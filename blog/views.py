66f11ab7feeddd7c4896484a7441e60c71d42406
Jaylen Dorsey <jaylendorsey@Jaylens-MacBook-Pro.local>
2024-12-19 22:39:28 -0600

    did some work in the shell and replaced our dummy code with actual POST objects

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from .models import Post
 
-
-
-posts= [
-    {
-    'author': 'Jaylen22',
-    'title': 'Blog Post 1',
-    'content':'First post content',
-    'date_posted': 'December 11, 2024'
-    },
-    {
-    'author': 'AlyssaCarter5',
-    'title': 'Blog Post 2',
-    'content':'Second post content',
-    'date_posted': 'December 1 , 2024'
-    }
-
-]
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html',context, )
 
