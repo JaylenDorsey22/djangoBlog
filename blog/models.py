ad7a783bed1e29f77b0a7d7b2c40728483c4c906
Jaylen Dorsey <jaylendorsey@Jaylens-MacBook-Pro.local>
2024-12-19 22:37:41 -0600

    created a dunder method that helps return real data instead of mock data

--- blog/models.py
+++ blog/models.py
@@ -10,5 +10,10 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete= models.CASCADE)
     
+    
+    def __str__(self):
+        return self.title
+    
+
 
 
